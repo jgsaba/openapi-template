23341cb2b4357b111d5bb57d6336a8ee5f394e9f
pve_jjorge <pve_jjorge@uolinc.com>
2022-01-30 10:46:24 -0300

    first

---
+++ src/main/java/com/template/openapispec/OpenapispecApplication.java
@@ -0,0 +1,13 @@
+package com.template.openapispec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapispecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapispecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/template/openapispec/OpenapispecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.openapispec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapispecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
